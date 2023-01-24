ed9de2cc3c2c287546d1ce3999ca6e38bc37ad56
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2023-01-23 23:08:37 -0800

    done with the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,21 @@
 <template>
-<div></div>
+  <footer id="footer" class="mb-3 p-2">
+    <div class="container">
+      <p class="text-center text-white">
+        Created with <font-awesome-icon icon="fa-solid fa-heart" class="text-danger" /> By
+        <a href="mailto:ariyibi.baseet@gmail.com" class="text-decoration-none"
+          >Ariyibi Baseet</a
+        >
+        &copy; Copyright Reserved <span>2023</span>
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script>
 export default {
-    name : "FooterArea"
-}
+  name: "FooterArea",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import router from './router'
 import store from './store'
 import {library} from "@fortawesome/fontawesome-svg-core"
 import { faHtml5, faCss3, faJs, faVuejs, faBootstrap, faGitAlt, faGithub, faLaravel, faTwitterSquare, faLinkedin, faWhatsappSquare } from '@fortawesome/free-brands-svg-icons'
-import { faShare, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faShare, faEnvelope, faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faHtml5, faCss3, faJs, faVuejs, faBootstrap, faGitAlt, faGithub, faLaravel, faShare, faTwitterSquare, faLinkedin, faWhatsappSquare, faEnvelope)
+library.add(faHtml5, faCss3, faJs, faVuejs, faBootstrap, faGitAlt, faGithub, faLaravel, faShare, faTwitterSquare, faLinkedin, faWhatsappSquare, faEnvelope, faHeart)
 
 createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
